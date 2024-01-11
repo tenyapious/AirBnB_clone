21c101e956e2bbde2ca30deb1724e9252fa372b1
tenyapious <tenyapious@gmail.com>
2024-01-11 13:50:32 +0000

    test if value for id, created_at and updated_at is none

--- pious/tests/test_models/test_base_model.py
+++ pious/tests/test_models/test_base_model.py
@@ -6,20 +6,25 @@ import unittest
 from datetime import datetime
 from models.base_model import BaseModel
 
+my_model = BaseModel()
+
 class TestUUID(unittest.TestCase):
+    def test_isnotnone(self):
+        self.assertIsNotNone(my_model.id) 
 
     def test_isstring(self):
-        my_model = BaseModel()
         self.assertTrue(isinstance(my_model.id, str))
 
 class TestCreatedAt(unittest.TestCase):
+    def test_isnotnone(self):
+        self.assertIsNotNone(my_model.created_at) 
 
     def test_isdatetime(self):
-        my_model = BaseModel()
         self.assertTrue(isinstance(my_model.created_at, datetime))
 
 class TestUpdatedAt(unittest.TestCase):
+    def test_isnotnone(self):
+        self.assertIsNotNone(my_model.updated_at) 
 
     def test_isdatetime(self):
-        my_model = BaseModel()
         self.assertTrue(isinstance(my_model.updated_at, datetime))
