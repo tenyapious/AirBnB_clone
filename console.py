8711eaddd1620f75e24f5289f0fb5c3b89a47edf
tenyapious <tenyapious@gmail.com>
2024-01-14 12:27:02 +0000

    Write documentation and refactor

--- console.py
+++ console.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Defines the HBnB console."""
 
 import cmd
 import shlex
@@ -14,23 +15,38 @@ from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
-    """Simple command processor example."""
+    """Defines the HolbertonBnB command interpreter.
+
+    Args:
+        prompt (str): The command prompt.
+    """
 
     prompt = "(hbnb) "
 
-    def do_create(self, class_name):
-        if len(class_name) == 0:
+    def do_create(self, className):
+        """ Create a new class object and print out the id.
+
+        Args:
+            className: The name of the class
+        """
+        if len(className) == 0:
             print("** class name missing **")
         else:
             try:
-                my_model = globals()[class_name]()
+                my_model = globals()[className]()
                 storage.save()
                 print(my_model.id)
             except KeyError:
                 print("** class doesn't exist **")
 
-    def do_show(self, line):
-        line_args = line.split()
+    def do_show(self, className_objId):
+        """ Display the string representation of a class object of a given id.
+
+        Args:
+           className_objId(str): The object's class name and id
+        """
+
+        line_args = className_objId.split()
         if len(line_args) == 0:
             print("** class name missing **")
         elif len(line_args) == 1:
@@ -54,8 +70,14 @@ class HBNBCommand(cmd.Cmd):
             else:
                 print("** class doesn't exist **")
 
-    def do_destroy(self, line):
-        line_args = line.split()
+    def do_destroy(self, className_objId):
+        """ Delete a class object of a given id
+
+        Args:
+           className_objId(str): The object's class name and id
+        """
+
+        line_args = className_objId.split()
         if len(line_args) == 0:
             print("** class name missing **")
         elif len(line_args) == 1:
@@ -82,22 +104,35 @@ class HBNBCommand(cmd.Cmd):
             else:
                 print("** class doesn't exist **")
 
-    def do_all(self, class_name=""):
+    def do_all(self, className=""):
+        """ Display string representations of all object of a given class
+
+        Args:
+           className(str): The object's class name
+        """
+
         objects = storage.all()
         list_objects = []
 
         if objects:
             for obj_key, obj_val in objects.items():
-                if not class_name or class_name == obj_val.__class__.__name__:
+                if not className or className == obj_val.__class__.__name__:
                     list_objects.append(str(obj_val))
 
-        if class_name and len(list_objects) == 0:
+        if className and len(list_objects) == 0:
             print("** class doesn't exist **")
         else:
             print(list_objects)
 
-    def do_update(self, line):
-        line_args = shlex.split(line)
+    def do_update(self, className_objId_attr_attr_val):
+        """ Update the attribute of a class object."
+
+        Args:
+           className_objId_attr_attr_val(str): The object's class name,
+           id, attribute to update and attribute value
+        """
+
+        line_args = shlex.split(className_objId_attr_attr_val)
         if len(line_args) == 0:
             print("** class name missing **")
         elif len(line_args) == 1:
@@ -136,9 +171,21 @@ class HBNBCommand(cmd.Cmd):
                 print("** class doesn't exist **")
 
     def do_quit(self, line):
+        """ Exit the program.
+
+        Returns:
+            True
+        """
+
         return True
 
     def do_EOF(self, line):
+        """ EOF signal to exit the program
+
+        Returns:
+            True
+        """
+
         return True
 
 
