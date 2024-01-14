cb0dc6ff5065589a6227528d3daced5546611fba
tenyapious <tenyapious@gmail.com>
2024-01-14 13:16:30 +0000

    Write documentation for base_model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,4 @@
 #!/user/bin/python3
-
 """ Define a test class TestBaseModel """
 import unittest
 from datetime import datetime
@@ -9,24 +8,36 @@ my_model = BaseModel()
 
 
 class TestUUID(unittest.TestCase):
+    """ Define TestUUID test class """
+
     def test_isnotnone(self):
+        """ Assert id is not None """
         self.assertIsNotNone(my_model.id)
 
     def test_isstring(self):
+        """ Assert id is a string """
         self.assertTrue(isinstance(my_model.id, str))
 
 
 class TestCreatedAt(unittest.TestCase):
+    """ Define TestCreatedAt test class """
+
     def test_isnotnone(self):
+        """ Assert created_at is not None """
         self.assertIsNotNone(my_model.created_at)
 
     def test_isdatetime(self):
+        """ Assert created_at is an instance of datetime """
         self.assertTrue(isinstance(my_model.created_at, datetime))
 
 
 class TestUpdatedAt(unittest.TestCase):
+    """ Define TestUpdatedAt test class """
+
     def test_isnotnone(self):
+        """ Assert updated_at is not None """
         self.assertIsNotNone(my_model.updated_at)
 
     def test_isdatetime(self):
+        """ Assert updated_at is a string """
         self.assertTrue(isinstance(my_model.updated_at, datetime))
