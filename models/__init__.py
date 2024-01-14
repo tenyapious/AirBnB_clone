9ac0a191d19ddf14012bb03a637c8600a24d3046
tenyapious <tenyapious@gmail.com>
2024-01-14 16:45:52 +0000

    Write documentation

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,24 @@
 #!/usr/bin/python3
+"""
+Package Initialization for 'models'
+
+This __init__.py file serves as the initialization file for the 'models' package.
+It includes the instantiation of the 'FileStorage' class and a 'storage' instance
+for handling persistent storage of model objects.
+
+Usage:
+    from models import storage
+
+    # Accessing the 'FileStorage' instance
+    storage_instance = storage
+
+    # Reloading data from storage
+    storage_instance.reload()
+"""
+
+from models.engine.file_storage import FileStorage
+storage = FileStorage()
+storage.reload()
 
 from models.engine.file_storage import FileStorage
 
